11301c4f44638745ee86f37fe97c5b8e3f5c5b6d
Rumit Shah <rumitshah1999@gmail.com>
2023-05-21 01:00:19 +0530

    Added cmd line args

--- hello.c
+++ hello.c
@@ -1,9 +1,14 @@
 
 #include <stdio.h>
+#include <stdint.h>
 
 
-int main()
+int main(int argc, char *argv[])
 {
+	if(argc > 1)
+	{
+		printf("arg1 is %s\n",argv[1]);
+	}
 	printf("hello world\n");
 	return 0;
 }
